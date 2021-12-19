c21ca8d6f19ae2358c634f0ee17c418047c307f9
Juan Carlos Flores Allende <carlitosallende99@hotmail.com>
2021-12-19 13:15:11 -0600

    subiendo todos los archivos

---
+++ README.md
@@ -0,0 +1,11 @@
+# APIPOKEMON
+Este es un reto hecho pra Curadeuda, utilizando la API de pokemon
+![image](https://user-images.githubusercontent.com/80058987/146686628-34e3c787-cf92-4893-a145-ba9bb23a114d.png)
+
+![image](https://user-images.githubusercontent.com/80058987/146686542-8a587272-5983-491e-9d28-d4d32540fc8b.png)
+Como se muestra en la parte de arriba así es como se ve nuestro proyecto, se le agregó un inicio en donde tenemos un menú desplegable y dice ahí quien hizo el proyecto, de la misma forma cuenta con un modal en donde nos dicen en que consiste este proyecto
+![image](https://user-images.githubusercontent.com/80058987/146686616-6e1fbc7d-787f-44ec-a85d-0feaa1106c2e.png)
+La página web funciona de manera que cada cuando se ingresa un id o algún nombre de un pokemon este debe de aparecer y ver que caracteristicas tiene 
+![image](https://user-images.githubusercontent.com/80058987/146686659-ab3812f2-ba85-4dde-9690-b0ce70a4fda4.png)
+![image](https://user-images.githubusercontent.com/80058987/146686673-b7786f2d-1df9-4625-b69a-ded0d2e35de8.png)
+Por último se puede observar que se cuenta con un botón flotante con el icono de una pokebola ya que si  oprimimos ese botón nos llevará a la API que estamos utilizando 
---
+++ js/scripts.js
@@ -0,0 +1,125 @@
+//seleccionar cada data mencionanda en el html
+const pokeCard = document.querySelector('[data-poke-card]');
+const pokeName = document.querySelector('[data-poke-name]');
+const pokeImg = document.querySelector('[data-poke-img]');
+const pokeImgContainer = document.querySelector('[data-poke-img-container]');
+const pokeId = document.querySelector('[data-poke-id]');
+const pokeTypes = document.querySelector('[data-poke-types]');
+const pokeStats = document.querySelector('[data-poke-stats]');
+
+//colores a utilizar
+const typeColors = {
+    electric: '#FFEA70',
+    normal: '#B09398',
+    fire: '#FF675C',
+    water: '#0596C7',
+    ice: '#AFEAFD',
+    rock: '#999799',
+    flying: '#7AE7C7',
+    grass: '#4A9681',
+    psychic: '#FFC6D9',
+    ghost: '#561D25',
+    bug: '#A2FAA3',
+    poison: '#795663',
+    ground: '#D2B074',
+    dragon: '#DA627D',
+    steel: '#1D8A99',
+    fighting: '#2F2F2F',
+    default: '#2A1A1F',
+};
+
+//crear funcion para el onsubmit
+const searchPokemon = event => {
+    event.preventDefault(); //se hace para cancelar el submit del for
+    const { value } = event.target.pokemon;
+    fetch(`https://pokeapi.co/api/v2/pokemon/${value.toLowerCase()}`)
+        .then(data => data.json())
+        .then(response => renderPokemonData(response))
+        .catch(err => renderNotFound())
+}
+
+//funcion en donde se van a revisar los datos
+const renderPokemonData = data => {
+    const sprite =  data.sprites.front_default;
+    const { stats, types } = data;
+
+    pokeName.textContent = data.name;
+    pokeImg.setAttribute('src', sprite);
+    pokeId.textContent = `Nº ${data.id}`;
+    setCardColor(types); //se va a utilizar para los fondos de cada pokemon
+    renderPokemonTypes(types);
+    renderPokemonStats(stats);
+}
+
+//utilizando la variable de arriba para los fondos de cada pokemon
+const setCardColor = types => {
+    const colorOne = typeColors[types[0].type.name];
+    const colorTwo = types[1] ? typeColors[types[1].type.name] : typeColors.default;
+    pokeImg.style.background =  `radial-gradient(${colorTwo} 33%, ${colorOne} 33%)`;
+    pokeImg.style.backgroundSize = ' 7px 7px';
+}
+
+//funcion para 
+const renderPokemonTypes = types => {
+    pokeTypes.innerHTML = '';
+    types.forEach(type => {
+        const typeTextElement = document.createElement("div");
+        typeTextElement.style.color = typeColors[type.type.name];
+        typeTextElement.textContent = type.type.name;
+        pokeTypes.appendChild(typeTextElement);
+    });
+}
+
+const renderPokemonStats = stats => {
+    pokeStats.innerHTML = '';
+    stats.forEach(stat => {
+        const statElement = document.createElement("div");
+        const statElementName = document.createElement("div");
+        const statElementAmount = document.createElement("div");
+        statElementName.textContent = stat.stat.name;
+        statElementAmount.textContent = stat.base_stat;
+        statElement.appendChild(statElementName);
+        statElement.appendChild(statElementAmount);
+        pokeStats.appendChild(statElement);
+    });
+}
+
+const renderNotFound = () => {
+    pokeName.textContent = 'No encontrado';
+    pokeImg.setAttribute('src', 'poke-shadow.png');
+    pokeImg.style.background =  '#fff';
+    pokeTypes.innerHTML = '';
+    pokeStats.innerHTML = '';
+    pokeId.textContent = '';
+}
+
+//Parte en donde se le da los efectos a la parte de inicio.html
+let boton = document.getElementById("icono");
+let enlaces = document.getElementById("enlaces");
+let contador = 0;
+
+boton.addEventListener("click", function(e){
+    e.preventDefault();
+    if(contador==0){
+        enlaces.className = ("enlaces dos")
+        contador=1;
+    }else{
+        enlaces.classList.remove("dos")
+        enlaces.className = ("enlaces uno")
+        contador=0;
+    }
+})
+
+(function() {
+    //a partir de que punto del scroll vertical de la ventana mostrará el botón
+    const ishow = 115
+    const $divtop = document.getElementById("div-totop")
+    window.addEventListener("scroll", function() {
+        if(document.documentElement.scrollTop > ishow){
+            $divtop.style.display = "inherit"
+        }
+        else {
+            $divtop.style.display = "none"
+        }
+    })
+})()
